c53198b94a1ee54cee6f800a68cce7ef1857623e
Mounik <mounicou@gmail.com>
2024-09-12 22:32:15 +0200

    Ajout de collection

--- readme.md
+++ readme.md
@@ -12,10 +12,14 @@
 
 ## Fonctionalitées
 
-[] Ajouter une collection  
-[] Supprimer une collection  
-[] Renommer une collection  
-[] Empêcher l'ajout d'une collection en doublon  
-[] Ajouter une tâche (relié à une collection)  
-[] Supprimer une tâche  
-[] Afficher les tâches d'une collection  
+[x] Ajouter une collection  
+[ ] Supprimer une collection  
+[ ] Renommer une collection  
+[ ] Empêcher l'ajout d'une collection en doublon  
+[ ] Ajouter une tâche (relié à une collection)  
+[ ] Supprimer une tâche  
+[ ] Afficher les tâches d'une collection  
+
+
+## Timestamps
+[DocString](https://www.youtube.com/watch?v=ns7cmSaiA9E&t=2527s)
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.utils.html import escape
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+
+from tasks.models import Collection
 
 
 # Create your views here.
 def index(request):
-    return render(request,'tasks/index.html', context={})
+    context = {}
+    context["collections"] = Collection.objects.order_by("name")
+
+    return render(request, 'tasks/index.html', context=context)
+
+def add_collection(request):
+    collection_name = escape(request.POST.get("collection-name"))
+    Collection.objects.create(name=collection_name)
+    return redirect('home')
